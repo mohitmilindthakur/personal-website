217f3e9c39e7c32cff52687e15e8725163a31435
Mohit Thakur <mohitmilindthakur@gmail.com>
2024-11-27 15:50:57 +0530

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+import "@/assets/css/main.css";
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+<h1>Loading...</h1>
+<img src="/images/loading.jpg   " alt="">
+</template>
+<script setup></script>
+
+<style scoped>
+img {
+    width: 50vw;
+}
+</style>
